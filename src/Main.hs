84373f006f54b22424ef53f741c771fdb13f935b
Jasper Weyne <jasperweyne@gmail.com>
2019-10-11 14:55:28 +0200

    Default code

--- src/Main.hs
+++ src/Main.hs
@@ -4,15 +4,17 @@ import Graphics.Gloss.Interface.IO.Game (Display(..), playIO)
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Game hiding (play, Event(..))
 
+import Controller
+import Model
+import View
+
 main :: IO ()
 main = do
     let screen = (1024, 768)
-    {-
-    playIO (InWindow "Game" screen (0, 0)) -- Screen
-            black                          -- Background color
-            30                             -- Frames per second
-            ?                              -- Model
-            ?                              -- View method
-            ?                              -- Event method
-            ?                              -- Step method
-            -}
+    playIO (InWindow "Game" screen (0, 0)) -- Or FullScreen
+              black            -- Background color
+              30               -- Frames per second
+              initialState     -- Initial state
+              view             -- View function
+              input            -- Event function
+              step             -- Step function

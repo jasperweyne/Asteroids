4e1bc0b1d59fc0df12da9f9b8c0f503233bb3896
Silvan <silvan.eelman@gmail.com>
2019-10-29 16:52:31 +0100

    Progress towards player input

--- src/Game/Player.hs
+++ src/Game/Player.hs
@@ -1,4 +1,10 @@
 module Game.Player where
   import Type.Object.Player
+  import Type.IO.Input
 
-  
+  updatePlayerControl :: Player -> GameState -> Player
+  updatePlayerControl p GameState{inputState = s} = case keyDown s Up of
+    True -> accelDir (vel o) (dir o) 5
+    False -> vel o
+    where
+      o = obj p

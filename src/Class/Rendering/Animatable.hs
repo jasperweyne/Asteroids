1c4ea777995c162d90c62a4b77d8669e305c60b7
Jasper Weyne <jasperweyne@gmail.com>
2019-10-29 16:10:51 +0100

    Delete unused files

--- src/Class/Rendering/Animatable.hs
+++
@@ -1,7 +0,0 @@
-module Class.Rendering.Animatable where
-
-    import Graphics.Gloss.Data.Picture
-
-    class (Drawable) => Animatable where
-        animation_index :: Integer
-        animation       :: [Picture]

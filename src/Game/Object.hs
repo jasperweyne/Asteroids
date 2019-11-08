3e0b7980dc1506970929351fea127dc0ddac30e4
Jasper Weyne <jasperweyne@gmail.com>
2019-11-08 12:44:07 +0100

    Improve parameter locality

--- src/Game/Object.hs
+++ src/Game/Object.hs
@@ -6,25 +6,24 @@ module Game.Object where
   import Type.IO.Input
   import Physics.Collisions
 
-  outOfBounds :: HasGameObject t => t -> GameState -> Either t t
+  outOfBounds :: GameObject -> GameState -> Either GameObject GameObject
   outOfBounds g GameState{inputState = s} | x >= 0 && x < w && y >= 0 && y < h = Right g
                                           | otherwise                          = Left wrapped
     where
-      wrapped = setGameObject g (getGameObject g) {
+      wrapped = g {
         pos = Pos {
           posX = (x `mod'` w) - w / 2,
           posY = (y `mod'` h) - h / 2
         }
       }
-      p = pos (getGameObject g)
-      x = w / 2 + posX p
-      y = h / 2 + posY p
+      x = w / 2 + posX (pos g)
+      y = h / 2 + posY (pos g)
       w = fromIntegral . fst $ screen s
       h = fromIntegral . snd $ screen s
 
   removeOutOfBounds :: HasGameObject t => t -> GameState -> Maybe t
-  removeOutOfBounds x gs = either (const Nothing) Just (outOfBounds x gs)
+  removeOutOfBounds x gs = either (const Nothing) (Just . setGameObject x) (outOfBounds (getGameObject x) gs)
 
   wrapOutOfBounds :: HasGameObject t => t -> GameState -> t
-  wrapOutOfBounds x gs = either id id (outOfBounds x gs)
+  wrapOutOfBounds x gs = either (setGameObject x) (setGameObject x) (outOfBounds (getGameObject x) gs)
                     

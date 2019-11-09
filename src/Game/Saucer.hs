04b71cfc01dc6ee5980310002941d4e2775d7873
Jasper Weyne <jasperweyne@gmail.com>
2019-11-09 13:29:28 +0100

    Remove IO from spawning saucers

--- src/Game/Saucer.hs
+++ src/Game/Saucer.hs
@@ -10,17 +10,17 @@ module Game.Saucer where
   postUpdateSaucers :: Float -> GameState -> GameState
   postUpdateSaucers t gs@GameState{inGame = igs@InGameState{saucers = s}} = updatedGs
     where
-      updatedGs | length removedSaucers == 0 = queueIO newGs spawnSaucer
-                | otherwise                  =         newGs
+      updatedGs | length sx == 0 = spawnSaucer newGs 
+                | otherwise      =             newGs
       newGs = gs{inGame = igs{
-        saucers = removedSaucers
+        saucers = sx
       }}
-      removedSaucers = mapMaybe (flip removeOutOfBounds $ gs) s
-
-  spawnSaucer :: GameState -> IO GameState
-  spawnSaucer gs@GameState{inGame = igs@InGameState{saucers = s}} = do
-    let (spawn, r) = spawnOnBounds (randGen gs) 100 gs
-    return gs{randGen = r, inGame = igs{
+      sx = map (`wrapOutOfBounds` gs) s
+      
+  spawnSaucer :: GameState -> GameState
+  spawnSaucer gs@GameState{inGame = igs@InGameState{saucers = s}} =
+    let (spawn, r) = spawnOnBounds (randGen gs) 100 gs in
+    gs{randGen = r, inGame = igs{
       saucers = Saucer {
         obj = spawn {
             radius = 25

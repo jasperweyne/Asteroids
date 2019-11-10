bf386ac2189f202ffad516c0ef050e93895211ec
Jasper Weyne <jasperweyne@gmail.com>
2019-11-10 15:39:37 +0100

    Move collision checks to pre-update

--- src/Game/Player.hs
+++ src/Game/Player.hs
@@ -12,9 +12,30 @@ module Game.Player (updatePlayer, postUpdatePlayer) where
   import Physics.Collisions
 
   updatePlayer :: Float -> GameState -> Player
-  updatePlayer t gs@GameState{inGame = igs@InGameState{player = p1, asteroids = as, sRockets = sx}} = p2
+  updatePlayer t gs@GameState{inGame = igs@InGameState{player = p1, asteroids = as, sRockets = sx}} = p4
     where
       p2 = updatePlayerControl p1 gs t
+      p3 = playerHitAsteroids p2 as
+      p4 = playerHitRockets p3 sx as
+  
+  playerHitAsteroids :: Player -> [Asteroid] -> Player
+  playerHitAsteroids p ast a
+    | p `collidesWith` ast = respawnPlayer p ast
+    | otherwise = p
+
+  playerHitRockets :: Player -> [Rocket] -> [Asteroid] -> Player
+  playerHitRockets p rx ast a
+    | p `collidesWith` rx = respawnPlayer p ast
+    | otherwise = p
+
+  respawnPlayer :: Player -> [Asteroid] -> Player
+  respawnPlayer p@Player{obj = o, lives = l} ast = p{lives = newLives, obj = newPlyObj}
+    where
+      newLives = l - 1
+      newPlyObj = foldl (\x y -> if collides x y then 
+          x{pos = pos x + toPos (offset (pos y) (pos x))} 
+        else x) o{pos = Pos 0 0, vel = Vel 0 0} asObjs 
+      asObjs = getGameObject <$> ast
 
   updatePlayerControl :: Player -> GameState -> Float -> Player
   updatePlayerControl p1@Player{obj = o} GameState{inputState = s} t = p2
@@ -42,32 +63,7 @@ module Game.Player (updatePlayer, postUpdatePlayer) where
 
   postUpdatePlayer :: Float -> GameState -> GameState
   postUpdatePlayer t gs@GameState{inGame = igs@InGameState{player = p, asteroids = as, sRockets = sx}} = gs{inGame = igs{
-    player = p4
+    player = p2
   }}
     where
       p2 = wrapOutOfBounds p gs
-      p3 = playerHitAsteroids p2 as
-      p4 = playerHitRockets p3 sx as
-
-  playerHitAsteroids :: Player -> [Asteroid] -> Player --reduce lives
-  playerHitAsteroids p ast
-    | p `collidesWith` ast = respawnPlayer p ast
-    | otherwise = p
-
-  playerHitRockets :: Player -> [Rocket] -> [Asteroid] -> Player
-  playerHitRockets p rx ast
-    | p `collidesWith` rx = respawnPlayer p ast
-    | otherwise = p
-
-  respawnPlayer :: Player -> [Asteroid] -> Player
-  respawnPlayer p@Player{obj = o, lives = l} ast = p{lives = newLives, obj = newPlyObj}
-    where
-      newLives = l - 1
-      newPlyObj = foldl (\x y -> if collides x y then 
-          x{pos = pos x + toPos (offset (pos y) (pos x))} 
-        else x) o{pos = Pos 0 0, vel = Vel 0 0} asObjs 
-      asObjs = getGameObject <$> ast
-      
-  --Create explosion if player lost life
-  -- ex2  | lives p2 < lives p1 = makeExplosion ((pos.getGameObject) p1) (explosion gs) : ex1 
-  --      | otherwise = ex1
